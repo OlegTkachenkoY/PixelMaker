625dfb8cdfec6c47c7b02e36f1b1c4cd954e485d
Oleg Tkachenko AKA Oblako <Oblako.tka@gmail.com>
2020-05-25 03:50:57 +0200

    Add class Filters(), add Class logic in Applying_filters.py

--- Applying_filters.py
+++ Applying_filters.py
@@ -1,38 +1,82 @@
-from PIL import Image, ImageDraw
 from datetime import datetime, date, time
 from tkinter import *
+from os import path
+from PIL import Image, ImageDraw
 
-apply_window = Tk()
+class Applying(object):
+    apply_window = Tk()
+    apply_window.title("Select a filter for each photo.")
+    apply_window.geometry('800x600')  # '800x600'
 
-apply_window.title("Select a filter for each photo.")
-apply_window.geometry('800x600')  # '800x600'
+    #def negativ_BtnAction():
 
+    def save(self, image, filter_name):
+        full_name = path.basename(self.getImage_path())
+        name = path.splitext(full_name)[0]
 
+        time_save = datetime.today().timetuple()
 
+        try:
+            image.save(
+                "Resources//({}){}_{}{}{}_{}{}{}{}.{}".format(
+                    name,
+                    filter_name,
+                    time_save[0],
+                    time_save[1],
+                    time_save[2],
+                    time_save[3],
+                    time_save[4],
+                    time_save[5],
+                    time_save[6],
+                    self.getFile_extension()))
+        except:
+            print("Error, file cannot save.")
+        else:
+            print("Saved successfully.")
+        finally:
+            del self.draw
 
+    def getImage_path(self):
+        return self.image_path
+
+    def setImage_path(self, image_path):
+        self.image_path = image_path
+
+    def getFile_extension(self):
+        return self.file_extension
+
+    def setFile_extension(self, file_extension):
+        self.file_extension = file_extension
+
+    image_path = 'Examples/2.jpg'
+    file_extension = 'png'  # jpg jpeg bmp png
+
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    pix = image.load()
 
 
 
 # Negative.
-btn_neg = Button(apply_window, text="Add a file", command=negative)
+btn_neg = Button(apply_window, text="Custom", command=custom(image, draw, pix))
 btn_neg.grid(column=1, row=0, sticky=W + E + N + S)
 # Grayscale.
-btn_gray = Button(apply_window, text="Add a file", command=gray)
-btn_gray.grid(column=1, row=0, sticky=W + E + N + S)
-# Sepia.
-btn_sepia = Button(apply_window, text="Add a file", command=sepia)
-btn_sepia.grid(column=1, row=0, sticky=W + E + N + S)
+# btn_gray = Button(apply_window, text="Add a file", command=gray)
+# btn_gray.grid(column=1, row=0, sticky=W + E + N + S)
+# # Sepia.
+# btn_sepia = Button(apply_window, text="Add a file", command=sepia)
+# btn_sepia.grid(column=1, row=0, sticky=W + E + N + S)
 # Brightness changes.
-btn_bright = Button(apply_window, text="Add a file", command=add_file)
-btn_bright.grid(column=1, row=0, sticky=W + E + N + S)
-# Contrast.
-btn_contrast = Button(apply_window, text="Add a file", command=add_file)
-btn_contrast.grid(column=1, row=0, sticky=W + E + N + S)
-# Black and white.
-btn_BandW = Button(apply_window, text="Add a file", command=add_file)
-btn_BandW.grid(column=1, row=0, sticky=W + E + N + S)
-# Adding noise.
-btn_noise = Button(apply_window, text="Add a file", command=add_file)
-btn_noise.grid(column=1, row=0, sticky=W + E + N + S)
+# btn_bright = Button(apply_window, text="Add a file", command=add_file)
+# btn_bright.grid(column=1, row=0, sticky=W + E + N + S)
+# # Contrast.
+# btn_contrast = Button(apply_window, text="Add a file", command=add_file)
+# btn_contrast.grid(column=1, row=0, sticky=W + E + N + S)
+# # Black and white.
+# btn_Black_White = Button(apply_window, text="Add a file", command=add_file)
+# btn_Black_White.grid(column=1, row=0, sticky=W + E + N + S)
+# # Adding noise.
+# btn_noise = Button(apply_window, text="Add a file", command=add_file)
+# btn_noise.grid(column=1, row=0, sticky=W + E + N + S)
 
 apply_window.mainloop()
---
+++ Filters.py
@@ -0,0 +1,161 @@
+import random
+
+class Filtters(object):
+    def negative(self, image, draw, pix):
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r, g, b = pix[i, j]
+
+                draw.point((i, j), (255 - r, 255 - g, 255 - b))
+
+    def gray(self, image, draw, pix):
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r, g, b = pix[i, j]
+
+                avg = (r + g + b) // 3
+
+                draw.point((i, j), (avg, avg, avg))
+
+    # 0 < and >100 ??
+    def sepia(self, image, draw, pix):
+        depth = int(input('depth:'))
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+
+                r, g, b = pix[i, j]
+
+                avg = (r + g + b) // 3
+
+                r = avg + depth * 2
+                g = avg + depth
+                b = avg
+
+                if r > 255: r = 255
+                if g > 255: g = 255
+                if b > 255: b = 255
+
+                draw.point((i, j), (r, g, b))
+        # image.save("Resources/ans.jpg", "JPEG")
+        # del draw
+
+    # < 0 and > 100
+    def bright(self, image, draw, pix):
+        factor = int(input('factor:'))
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r = factor + pix[i, j][0]
+                g = factor + pix[i, j][1]
+                b = factor + pix[i, j][2]
+
+                if r < 0: r = 0
+                if g < 0: g = 0
+                if b < 0: b = 0
+
+                if r > 255: r = 255
+                if g > 255: g = 255
+                if b > 255: b = 255
+
+                draw.point((i, j), (r, g, b))
+        # image.save("Resources/ans.jpg", "JPEG")
+        # del draw
+
+    # < -50 and >50
+    def contrast(self, image, draw, pix):
+        coefficient = int(input("coefficient: ")) / 2
+
+        avg = 0
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r, g, b = pix[i, j]
+
+                avg += (r * 0.299 + g * 0.587 + b * 0.114)
+
+        avg /= image.size[0] * image.size[1]
+
+        palette = []
+        for i in range(256):
+
+            temp = int(avg + coefficient * (i - avg))
+
+            if temp < 0: temp = 0
+            if temp > 255: temp = 255
+
+            palette.append(temp)
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r, g, b = pix[i, j]
+
+                draw.point((i, j), (palette[r], palette[g], palette[b]))
+
+        # image.save("Resources/ans.jpg", "JPEG")
+        # del draw
+
+    # < -150 and >150
+    def black_white(self, image, draw, pix):
+        factor = int(input('factor:'))
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                r, g, b = pix[i, j]
+
+                avg = r + g + b
+
+                if avg > (((255 + factor) // 2) * 3):
+                    r, g, b = 255, 255, 255
+                else:
+                    r, g, b = 0, 0, 0
+                draw.point((i, j), (r, g, b))
+        # image.save("Resources/ans.jpg", "JPEG")
+        # del draw
+
+    # < 0 and > 1000
+    def noise(self, image, draw, pix):
+        factor = int(input('factor:'))
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+
+                rand = random.randint(-factor, factor)
+
+                r = pix[i, j][0] + rand
+                g = pix[i, j][1] + rand
+                b = pix[i, j][2] + rand
+
+                if r < 0: r = 0
+                if g < 0: g = 0
+                if b < 0: b = 0
+
+                if r > 255: r = 255
+                if g > 255: g = 255
+                if b > 255: b = 255
+                draw.point((i, j), (r, g, b))
+        # image.save("Resources/ans.jpg", "JPEG")
+        # del draw
+
+    def custom(self, image, draw, pix):
+        depth = int(input('depth:'))
+
+        r1 = int(input("R: "))
+        g1 = int(input("G: "))
+        b1 = int(input("B: "))
+
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+
+                r, g, b = pix[i, j]
+
+                r += r1 + depth
+                g += g1 + depth
+                b += b1 + depth
+
+                if r > 255: r = 255
+                if g > 255: g = 255
+                if b > 255: b = 255
+
+                draw.point((i, j), (r, g, b))
+
